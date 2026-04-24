d0a2d70d1b2cfe8fa59fbd9c915322cd0accefa9
Scelo <scelogumede95@gmail.com>
2026-04-24 21:39:52 +0200

    finished model

--- backend/community/community/settings.py
+++ backend/community/community/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'com.apps.ComConfig'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'community.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'community',
+        'USER': 'postgres',
+        'PASSWORD': '72664453',
+        'HOST': 'localhost',  # or your DB host
+        'PORT': '5432',
     }
 }
 
