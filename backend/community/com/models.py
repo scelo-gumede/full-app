0cc36ef69620cd97107fe25deff8b9a94dbabc7d
Scelo <scelogumede95@gmail.com>
2026-04-24 21:05:37 +0200

    created model

--- backend/community/com/models.py
+++ backend/community/com/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Community(models.Model):
+
+    parties={
+        "ANC":"African National Congress",
+        "IFP":"Inkatha Freedom Party",
+        "EFF":"Economic Freedom Fighter",
+    }
+
+    person_name=models.CharField(max_length=50)
+    person_surname=models.CharField(max_length=50)
+    person_area=models.IntegerField()
+    person_party=models.CharField(max_length=5,choices=parties)
