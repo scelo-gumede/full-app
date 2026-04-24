77657c0c5e8c0f721d09f3102f1ff5b921aa6687
Scelo <scelogumede95@gmail.com>
2026-04-24 23:25:17 +0200

    added create route

--- backend/community/com/views.py
+++ backend/community/com/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from . import models
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 
-
+@csrf_exempt
 def create(request):
-    return HttpResponse("that the creation")
+
+    print(request.body)
+    data=json.loads(request.body)
+    name=data.get("name")
+    surname=data.get("surname")
+    area=data.get("area")
+    party=data.get("party")
+    create_person=models.Community(person_name=name,person_surname=surname,person_area=area,person_party=party)
+    create_person.save()
+
+    return JsonResponse({
+        "data":"the item is created"
+    },safe=False)
 
 
 def get_list(request):
